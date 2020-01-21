2a8c3f2773763ed97ef440fab52a6f80036037ed
Michael Rehn <rehn.michael@outlook.com>
2020-01-21 16:29:28 +0100

    Add action

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const tagToDelete = core.getInput("tag");
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+    const octokit = new github.GitHub(GITHUB_TOKEN);
+    const context = github.context;
+
+    await octokit.git.deleteRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: tagToDelete
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
